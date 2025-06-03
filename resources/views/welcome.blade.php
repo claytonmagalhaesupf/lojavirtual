6818d48aa2269f6a4b004903eede42697adbc33d
clayton <claytonmagalhaes@upf.br>
2025-06-03 01:04:10 -0300

    feat: :sparkles: add breeze feature and front customize

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,3 @@
-<p>Seja bem vindo ao sistema de controle de estoque!</p>
-<ul>
-    <li><a href="{{ url('/products') }}">Lista de produtos</a></li>
-    <li><a href="{{ url('/types') }}">Tipos de produtos</a></li>
-</ul>
+<x-guest-layout >
+Bem vindo
+</x-guest-layout>
